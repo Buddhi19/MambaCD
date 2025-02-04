070c8459ae51f17437621c11e1bbdb487921f84a
Buddhi19 <wbuddhi@icloud.com>
2025-02-04 13:22:46 +0530

    Refactor prediction and label calculations in Trainer class to streamline accuracy updates and improve clarity

--- changedetection/script/train_MambaSCD.py
+++ changedetection/script/train_MambaSCD.py
@@ -205,8 +205,6 @@ class Trainer(object):
                 # input_data = torch.cat([pre_change_imgs, post_change_imgs], dim=1)
                 output_1, output_semantic_t1, output_semantic_t2 = self.deep_model(pre_change_imgs, post_change_imgs)
 
-                change_mask = torch.argmax(output_1, axis=1)
-
                 labels_cd = labels_cd.cpu().numpy()
                 labels_A = labels_clf_t1.cpu().numpy()
                 labels_B = labels_clf_t2.cpu().numpy()
@@ -216,31 +214,19 @@ class Trainer(object):
                 preds_A = torch.argmax(output_semantic_t1, dim=1).cpu().numpy()
                 preds_B = torch.argmax(output_semantic_t2, dim=1).cpu().numpy()
                 
-                preds_A = (preds_A* change_mask.squeeze().long()).numpy()
-                preds_B = (preds_B* change_mask.squeeze().long()).numpy()
 
-                for (pred_A, pred_B, label_A, label_B) in zip(preds_A, preds_B, labels_A, labels_B):
-                    acc_A, valid_sum_A = accuracy(pred_A, label_A)
-                    acc_B, valid_sum_B = accuracy(pred_B, label_B)
-                    preds_all.append(pred_A)
-                    preds_all.append(pred_B)
-                    labels_all.append(label_A)
-                    labels_all.append(label_B)
-                    acc = (acc_A + acc_B) * 0.5
+                preds_scd = (preds_A - 1) * 6 + preds_B
+                preds_scd[change_mask == 0] = 0
+
+                labels_scd = (labels_A - 1) * 6 + labels_B
+                labels_scd[labels_cd == 0] = 0
+
+                for (pred_scd, label_scd) in zip(preds_scd, labels_scd):
+                    acc_A, valid_sum_A = accuracy(pred_scd, label_scd)
+                    preds_all.append(pred_scd)
+                    labels_all.append(label_scd)
+                    acc = acc_A
                     acc_meter.update(acc)
-
-                # preds_scd = (preds_A - 1) * 6 + preds_B
-                # preds_scd[change_mask == 0] = 0
-
-                # labels_scd = (labels_A - 1) * 6 + labels_B
-                # labels_scd[labels_cd == 0] = 0
-
-                # for (pred_scd, label_scd) in zip(preds_scd, labels_scd):
-                #     acc_A, valid_sum_A = accuracy(pred_scd, label_scd)
-                #     preds_all.append(pred_scd)
-                #     labels_all.append(label_scd)
-                #     acc = acc_A
-                #     acc_meter.update(acc)
 
         kappa_n0, Fscd, IoU_mean, Sek = SCDD_eval_all(preds_all, labels_all, 37)
         print(f'Kappa coefficient rate is {kappa_n0}, F1 is {Fscd}, OA is {acc_meter.avg}, '
